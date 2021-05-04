2543de64dad84052f492b893e72aafdadabe6c82
Applevena <68597216+Apple-Book@users.noreply.github.com>
2021-05-04 14:28:31 +0900

    Create ABC 196

---
+++ ABC 196/c.swift
@@ -0,0 +1,44 @@
+//
+//  Using Swift 5.0
+//
+
+import Foundation
+
+// MARK: Main Solution
+//
+func solution() {
+	let N: Int = readInt()
+	
+	var count: Int = 0
+	
+	// O(10^6)
+	for n in 1...1000_000 {
+		let num = Int("\(n)\(n)")!
+		
+		guard num <= N else { break }
+		
+		count += 1
+	}
+	
+	print(count)
+}
+
+
+solution()
+
+
+// MARK: Utils
+//
+func readLine<T: StringProtocol>(separatedBy separator: T) -> [String] { readLine()!.components(separatedBy: separator) }
+
+func readJoinedLine() -> [String] { readLine()!.map { String($0) } }
+
+func readInt() -> Int { Int(readLine()!)! }
+
+func readInts() -> [Int] { readLine()!.components(separatedBy: " ").map { Int($0)! } }
+
+prefix operator «
+prefix func « <T> (a: [T]) -> (T, T) { (a[0], a[1]) }
+prefix func « <T> (a: [T]) -> (T, T, T) { (a[0], a[1], a[2]) }
+prefix func « <T> (a: [T]) -> (T, T, T, T) { (a[0], a[1], a[2], a[3]) }
+prefix func « <T> (a: [T]) -> (T, T, T, T, T) { (a[0], a[1], a[2], a[3], a[4]) }
